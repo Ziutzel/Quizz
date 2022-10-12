47d06606ede777af5be1648d8c748f55c0b10d38
ZIUTZELLE GRAJALES <ziutzelg@gmail.com>
2022-10-12 14:49:12 -0500

    Initial Commit

---
+++ QUIZZ/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  QUIZZ
+//
+//  Created by Ziutzel grajales on 12/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
